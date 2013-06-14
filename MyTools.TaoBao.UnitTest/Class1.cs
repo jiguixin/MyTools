19de7f2e75809869832e89804cfff322939bed8c
jiguixin <jiguixin_1008@qq.com>
2013-06-14 16:57:59 +0800

    增加产品详情CSS样式的实现

--- MyTools.TaoBao.UnitTest/Class1.cs
+++ MyTools.TaoBao.UnitTest/Class1.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using HtmlAgilityPack;
 using Infrastructure.Crosscutting.Utility.CommomHelper;
 using NUnit.Framework;
+using System.Text.RegularExpressions;
+using Infrastructure.Crosscutting.Declaration;
 
-namespace MyTools.TaoBao.UnitTest
+namespace UnitTest
 {
     [TestFixture]
     public class Class1
@@ -73,6 +76,120 @@ namespace MyTools.TaoBao.UnitTest
         {
 
         }
-         
+
+
+        [Test]
+        public void FooTest()
+        {
+            #region var
+
+            string reg = @"[\.\#]?\w+[^{]+\{[^}]*\}";
+
+            #endregion
+
+            #region GetContent
+
+            string htmlContent = FileHelper.ReadFileContent(@"C:\Users\Administrator\Desktop\abc.html");
+            string cssContent = FileHelper.ReadFileContent(@"C:\Users\Administrator\Desktop\abc.css");
+
+            #endregion
+
+            HtmlDocument doc = new HtmlDocument();
+
+            doc.LoadHtml(htmlContent);
+
+            var lstCss = GetCssContent(cssContent);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var css in lstCss)
+            {
+                #region Get Xpath
+                   
+                var l = css.IndexOf("{", System.StringComparison.Ordinal);
+
+                var token = css.Substring(0, l);
+                var sytle = css.Substring(l + 1, css.Length - l - 2);
+                 
+                sb.Clear();
+
+                var singleElement = token.Split(' ');
+
+                foreach (var s in singleElement)
+                {
+                    ConstructXpath(s, sb);
+                }
+
+                #endregion
+                 
+               var nodes =  doc.DocumentNode.SelectNodes(sb.ToString());
+
+                if (nodes.IsNull())
+                    continue;
+
+                foreach (var node in nodes)
+                {
+                    node.Attributes.Add("style", sytle); 
+                }
+            }
+
+            Console.WriteLine(doc.DocumentNode.OuterHtml);
+
+        }
+
+        private static void ConstructXpath(string s, StringBuilder sb)
+        {
+            if (s.IsNullOrEmpty())
+                return;
+
+            if (s.Contains("#"))
+            {
+                var ele = s.Replace("#", "");
+                 
+                sb.Append("//*[@id=\"{0}\"]".StringFormat(ele));
+                 
+                return;
+            }
+
+            if (s.Contains("."))
+            {
+                var ele = s.Replace(".", "");
+                sb.Append("//*[@class=\"{0}\"]".StringFormat(ele));
+                return;
+            }
+
+            if (sb.Length > 0)
+                sb.Append("/");
+            sb.Append(s);
+        }
+
+        #region Helper
+
+        public static List<string> GetCssContent(string source)
+        {
+            //匹配CSS文件中的CSS样式
+            var reg = @"[\.\#]?\w+[^{]+\{[^}]*\}";
+            var lstResult = new List<string>();
+            if (!string.IsNullOrEmpty(source))
+            {
+                var r = new Regex(reg); //定义一个Regex对象实例
+                var mc = r.Matches(source);
+                for (var i = 0; i < mc.Count; i++) //在输入字符串中找到所有匹配
+                {
+                    string val = mc[i].Value;
+
+                    if (val.IsNullOrEmpty())
+                        continue;
+
+                    lstResult.Add(val.Trim()); 
+                }
+            }
+            return lstResult;
+        }
+
+        #endregion
+
+
+
     } 
 }
